622c23cfd6df76c5b2ff4b84ae84219b0d94a041
carlysse <92702771+carlysse@users.noreply.github.com>
2022-04-24 03:13:24 -0700

    created advanced level and initialized MainMenu and winScene

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; 
+using UnityEngine.SceneManagement; 
 
 public class Score : MonoBehaviour
 {
     public static int currentScore = 0; 
+    public int goal = 10; 
 
     public Text scoreText; 
     void Start ()
     {
         scoreText.text = currentScore.ToString(); 
+        if (currentScore >= goal) 
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+
     }
 
 
